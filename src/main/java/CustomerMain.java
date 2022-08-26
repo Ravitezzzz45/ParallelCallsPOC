9ff88862e6f763873a50a8eefedb38200c697434
Ravitezzzz45 <ravitejapolepalli.1996@gmail.com>
2022-08-26 23:18:18 +0530

    changes made for feature brnch

--- src/main/java/CustomerMain.java
+++ src/main/java/CustomerMain.java
@@ -2,7 +2,7 @@ import com.sreenutech.model.CustomerDetails;
 import com.sreenutech.service.CustomerService;
 
 import java.util.concurrent.ExecutionException;
-
+// hey made changes
 public class CustomerMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         CustomerService service= new CustomerService();
